e3e84051b1ac8efba55267480d650f0a63b96ad6
Zach <zacht@anglepoint.com>
2016-05-31 00:45:36 -0600

    contact form fix

--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,7 @@ var routes = {
 exports = module.exports = function (app) {
 	// Views
 	app.get('/', routes.views.index);
+	app.post('/', routes.views.contact);
 	app.get('/blog/:category?', routes.views.blog);
 	app.get('/blog/post/:post', routes.views.post);
 	app.get('/gallery', routes.views.gallery);
