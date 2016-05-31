0ad997857aab4fcac59b50c032bc5efbf3725566
Zach <zacht@anglepoint.com>
2016-05-30 21:36:44 -0600

    Sunshine Interior Design Site v1 commit

---
+++ routes/views/index.js
@@ -0,0 +1,46 @@
+var keystone = require('keystone');
+var Enquiry = keystone.list('Enquiry');
+
+exports = module.exports = function(req, res) {
+
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+
+	// locals.section is used to set the currently selected
+	// item in the header navigation.
+	locals.section = 'home';
+
+	// Load the galleries by sortOrder
+	view.query('galleries', keystone.list('Gallery').model.find().limit(4).sort('sortOrder'));
+
+	// Set locals
+	locals.enquiryTypes = Enquiry.fields.enquiryType.ops;
+	locals.formData = req.body || {};
+	locals.validationErrors = {};
+	locals.enquirySubmitted = false;
+
+	// On POST requests, add the Enquiry item to the database
+	view.on('post', {
+		action: 'contact'
+	}, function(next) {
+
+		var newEnquiry = new Enquiry.model();
+		var updater = newEnquiry.getUpdateHandler(req);
+
+		updater.process(req.body, {
+			flashErrors: true,
+			fields: 'name, email, phone, enquiryType, message',
+			errorMessage: 'There was a problem submitting your enquiry:',
+		}, function(err) {
+			if (err) {
+				locals.validationErrors = err.errors;
+			} else {
+				locals.enquirySubmitted = true;
+			}
+			next();
+		});
+	});
+
+	// Render the view
+	view.render('index');
+};
